ba608d2b466f1050e80fd80f040e5c8005320881
XyeD97 <s.m.alinqvi97@gmail.com>
2024-10-01 17:10:39 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Cricket Analysis app for EDA')
 
 st.write('Hello world!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/XyeD97/task3-ml/refs/heads/master/ODI_Match_info.csv')
