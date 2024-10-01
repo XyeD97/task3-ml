e7d91344e963ec2457f2ad8400bb3d80d1970769
XyeD97 <s.m.alinqvi97@gmail.com>
2024-10-01 17:27:50 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,5 @@ with st.expander('Data'):
   st.write('**Y**')
   y = df.id
   y
+with st.expander('Data Visualization')
+  st.scatter_chart(data= df, x='toss_winner', y= 'toss_decision', color='id')
