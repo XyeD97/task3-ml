f07a8aa125901b4644a8fae485bee548811074fa
XyeD97 <s.m.alinqvi97@gmail.com>
2024-10-01 17:15:20 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title('Cricket Analysis app for EDA')
 
 st.info('This app builds a machine learning model on cricket related dataset')
 
-df = pd.read_csv('https://raw.githubusercontent.com/XyeD97/task3-ml/refs/heads/master/ODI_Match_info.csv')
-df
+with st.expander('Data'):
+  st.write('**Raw Data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/XyeD97/task3-ml/refs/heads/master/ODI_Match_info.csv')
+  df
